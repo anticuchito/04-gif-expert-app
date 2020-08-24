5a38aa47396280e9c9343484ff1503bbdd1584dc
macroo14gmail.com <macroo14@gmail.com>
2020-08-24 01:26:36 -0400

    first commit

---
+++ docs/precache-manifest.1c5dc383dd794518ddf3ad13b11ac289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9993655ed73f2c186b064dbf902db60b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082f7d092e89603dd4da",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082f7d092e89603dd4da",
+    "url": "/static/js/main.a774f58b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
